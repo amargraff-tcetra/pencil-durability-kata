9a0f5e1b6420cc26839ce460e15d2b8c4ccffe42
Aaron Margraff <amargraff@tcetra.com>
2024-04-15 13:47:27 -0400

    Add paper

--- pencil-durability-kata-tests/PencilTests.cs
+++ pencil-durability-kata-tests/PencilTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using pencil_durability_kata;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ pencil-durability-kata/Paper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pencil_durability_kata
+{
+    public class Paper
+    {
+        public string Text { get; set; } = string.Empty;
+    }
+}
